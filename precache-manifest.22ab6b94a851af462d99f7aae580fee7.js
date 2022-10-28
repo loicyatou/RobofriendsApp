435011cb4d280cfa79e33300cf9abf7f97a80c30
Loic Yatou <Loic@LoicYatsMacBook.ht.home>
2022-10-28 15:25:20 +0100

    Updates

---
+++ precache-manifest.22ab6b94a851af462d99f7aae580fee7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "13a6b866c2cafebbbed3",
+    "url": "/RobofriendsApp/static/css/main.85bd2022.chunk.css"
+  },
+  {
+    "revision": "13a6b866c2cafebbbed3",
+    "url": "/RobofriendsApp/static/js/main.13a6b866.chunk.js"
+  },
+  {
+    "revision": "84cf99745ce0980187aa",
+    "url": "/RobofriendsApp/static/js/1.84cf9974.chunk.js"
+  },
+  {
+    "revision": "6f7446c134066e0cf85e",
+    "url": "/RobofriendsApp/static/css/2.c2dadeaf.chunk.css"
+  },
+  {
+    "revision": "6f7446c134066e0cf85e",
+    "url": "/RobofriendsApp/static/js/2.6f7446c1.chunk.js"
+  },
+  {
+    "revision": "8ab869d075cfcedf51d9",
+    "url": "/RobofriendsApp/static/js/runtime~main.8ab869d0.js"
+  },
+  {
+    "revision": "3747610fcbce19f8bd5a975a190c2013",
+    "url": "/RobofriendsApp/static/media/FerroRosso.3747610f.woff"
+  },
+  {
+    "revision": "69c41c173f067efd3cdb363322a3ae6c",
+    "url": "/RobofriendsApp/index.html"
+  }
+];
